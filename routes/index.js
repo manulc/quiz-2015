89633088777953f1943fe8784f289aef4766ffcb
manulc <manuel.lorenzo.cora@gmail.com>
2015-07-14 15:20:10 +0200

    Página del autor

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,13 @@ router.get('/', function(req, res) {
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
 
+router.get('/author',function(req,res){
+  res.render('author',{ 
+	nombre: 'Manuel L. Cora',
+	imagenWidth: 75,
+	imagenHeight: 75,
+	imagenUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRVa6w4um92HhzFPWrZxGpht51tQwLeTI9bDWZswCaiUPwW-NUKRA'
+  });
+});
+
 module.exports = router;
