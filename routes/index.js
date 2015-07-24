f3b19606ac00e357dc80e9ab4783ed7d28ca8caf
manulc <manuel.lorenzo.cora@gmail.com>
2015-07-24 15:04:52 +0200

    Validación de entradas - Corrección en la pantalla Autor

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,8 @@ router.get('/author',function(req,res){
 	nombre: 'Manuel L. Cora',
 	imagenWidth: 75,
 	imagenHeight: 75,
-	imagenUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRVa6w4um92HhzFPWrZxGpht51tQwLeTI9bDWZswCaiUPwW-NUKRA'
+	imagenUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRVa6w4um92HhzFPWrZxGpht51tQwLeTI9bDWZswCaiUPwW-NUKRA',
+  errors: []
   });
 });
 
