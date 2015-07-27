39626cd6140f0604f64a5b43934d70f5fae9cab2
manulc <manuel.lorenzo.cora@gmail.com>
2015-07-27 12:51:41 +0200

    Sequelize - función sync con then

--- models/models.js
+++ models/models.js
@@ -30,7 +30,7 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exportar la definicion de la tabla Quiz
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en DB
-sequelize.sync().success(function(){
+sequelize.sync().then(function(){
   // success(..) ejecuta el manejador una vez creada la tabla
   Quiz.count().success(function(count){
     if(count === 0){ // la tabla se inicializa solo si esta vacia
